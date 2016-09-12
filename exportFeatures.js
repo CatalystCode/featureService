bb133bbc9ce4c7797c39a3ae859d48f9e891ff3e
Tim Park <timfpark@gmail.com>
2016-09-12 16:00:06 -0700

    add exportFeatures script

---
+++ exportFeatures.js
@@ -0,0 +1,107 @@
+'use strict';
+
+let pbf2json = require('pbf2json'),
+    services = require('./services'),
+    through = require('through2'),
+    Tile = require('geotile');
+
+const FULL_TAGS_TO_CAPTURE = [
+    /*
+  'building:hut',
+  'historic:boundary_stone',
+  'historic:castle',
+  'historic:monument',
+  'historic:milestone',
+  'historic:rune_stone',
+  'man_made:beacon',
+  */
+  'natural:peak',
+/*
+  'iata:*',
+  'tourism:artwork',
+  'tourism:alpine_hut',
+  'tourism:wilderness_hut',
+  'tourism:viewpoint'
+*/
+];
+
+const FEATURE_TAGS = [
+/*  'aeroway',
+  'building',
+  'historic',
+  'iata',
+  'man_made', */
+  'natural' /*,
+  'tourism' */
+];
+
+let config = {
+    file: process.env.OSM_FILE,
+    tags: FEATURE_TAGS
+};
+
+let count = 0;
+pbf2json.createReadStream(config).pipe(
+    through.obj( (item, e, next) => {
+        let feature = {
+            id: item.id
+        };
+
+        FEATURE_TAGS.forEach( (featureTag) => {
+            if (!item.tags[featureTag]) return;
+
+            feature.category = featureTag;
+            feature.tag = item.tags[featureTag];
+            feature.fullTag = `${featureTag}:${item.tags[featureTag]}`;
+        });
+
+        if (FULL_TAGS_TO_CAPTURE.indexOf(feature.fullTag) < 0)
+            return next();
+
+        feature.names = {};
+        if (item.tags['name']) {
+            feature.names.common = item.tags['name'];
+        }
+
+        if (item.type === 'node') {
+            let latitude = parseFloat(item.lat);
+            let longitude = parseFloat(item.lon);
+
+            feature.centroid = {
+                "type":"Point",
+                "coordinates":[
+                    longitude,
+                    latitude
+                ]
+            };
+
+            let bboxDelta = 0.005;
+            feature.geometry = {
+                "type":"Polygon",
+                "coordinates":[[
+                    [longitude - bboxDelta, latitude + bboxDelta],
+                    [longitude + bboxDelta, latitude + bboxDelta],
+                    [longitude + bboxDelta, latitude - bboxDelta],
+                    [longitude - bboxDelta, latitude - bboxDelta],
+                    [longitude - bboxDelta, latitude + bboxDelta]
+                ]]
+            };
+
+            feature.adminLevel = 9999;
+
+            if (item.tags['ele']) {
+                feature.elevation = parseFloat(item.tags['ele']);
+            }
+        }
+
+        if (feature.geometry && feature.centroid && feature.elevation) {
+            let featureJson = JSON.stringify(feature);
+            console.log(featureJson);
+        }
+
+        next();
+    })
+).on('finish', () => {
+    console.log('finished');
+    setTimeout(process.exit, 5000);
+});
