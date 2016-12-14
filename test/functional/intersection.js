744516d9072c2f30b5185b17c380115875c566fe
Tim Park <timfpark@gmail.com>
2016-12-14 12:17:28 -0800

    add test for intersection endpoint

---
+++ test/functional/intersection.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const assert = require('assert');
+const app = require('../../server');
+const fixtures = require('../fixtures');
+const HttpStatus = require('http-status-codes');
+const request = require('request');
+
+const intersectionEndpoint = 'http://localhost:' + process.env.PORT + '/intersection';
+
+describe('features endpoint', function() {
+    it('can post intersection for processing', function(done) {
+        request.post(intersectionEndpoint, {
+            headers: {
+                Authorization: "Bearer " + fixtures.accessToken
+            },
+            body: {
+                intersection: fixtures.intersection
+            },
+            json: true
+        }, function(err, resp) {
+            assert(!err);
+            assert.equal(resp.statusCode, HttpStatus.ACCEPTED);
+
+            done();
+        });
+    });
+});
