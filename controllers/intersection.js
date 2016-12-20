9fd2a3b9375a64e3d4ac34e8b143b4cfa7dd940f
Tim Park <timfpark@gmail.com>
2016-12-20 12:28:23 -0800

    add info for new intersection

--- controllers/intersection.js
+++ controllers/intersection.js
@@ -5,6 +5,7 @@ const common = require('service-utils'),
       services = require('../services');
 
 exports.post = function(req, res) {
+    common.services.log.info('intersection received: ' + JSON.stringify(req.body.intersection, null, 2));
     services.visits.updateVisitsFromIntersection(req.body.intersection, err => {
         if (err) return common.utils.handleError(res, err);
 
