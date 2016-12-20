e7058191aa190e38f32511cbd5ee84701a06089a
Tim Park <timfpark@gmail.com>
2016-12-20 10:53:57 -0800

    import all counties in US and filter on type not place in hierarchy

--- tools/importWhosOnFirst.js
+++ tools/importWhosOnFirst.js
@@ -4,12 +4,54 @@ let async = require('async'),
     fs = require('fs'),
     path = require('path'),
     services = require('../services'),
-    Tile = require('geotile');
+    Tile = require('geotile'),
+    turf = require('turf');
 
 let DIRECTORY_PARALLELISM = 3;
 
 let skipped = 0;
 let total = 0;
+
+let skipIds = [
+  '0',
+  '1',
+  '85788865',
+  '85880723',
+  '85880767',
+  '85886313',
+  '85888131',
+  '404529565',
+  '404528711',
+  '420551843',
+  '420552373',
+  '420552465',
+  '420552969',
+  '420553211',
+];
+
+let keepIds = [
+
+];
+
+let ignoreTypes = [
+    'locality',
+    'localadmin',
+    'marinearea',
+    'macrohood',
+    'microhood',
+    'neighbourhood',
+    'ocean',
+    'planet',
+    'postalcode',
+    'timezone',
+];
+
+
+let countyCountries = [
+    85633793
+];
+
+let foundTypes = {};
 
 function processFile(file, callback) {
     fs.readFile(file, (err, data) => {
@@ -69,9 +111,33 @@ function processFile(file, callback) {
             feature.hierarchy = json['properties']['wof:hierarchy'][0];
         }
 
-        if (json['properties']['wof:belongsto'].length > 2) {
+        let placeType = json['properties']['wof:placetype'];
+        let squareKm = turf.area(feature.hull) / 1000000.0;
+
+        if (skipIds.indexOf(json.id) !== -1) {
+            console.log(json.id + ' is in kill list, skipping.');
+            skipped += 1;
             return callback();
         }
+
+        if (ignoreTypes.indexOf(placeType) !== -1) {
+            skipped += 1;
+            return callback();
+        }
+
+        if (placeType === "county" && (!feature.hierarchy || feature.hierarchy['country_id'] !== 85633793)) {
+            skipped += 1;
+            return callback();
+        }
+
+//        if (!foundTypes[placeType]) {
+//            console.log(placeType + ": " + json['properties']['wof:name']);
+//            foundTypes[placeType] = true;
+//        }
+
+//        if (json['properties']['wof:belongsto'].length > 2) {
+//            return callback();
+//        }
 
         services.features.upsert(feature, (err, feature) => {
             if (err) {
@@ -79,7 +145,7 @@ function processFile(file, callback) {
                 skipped += 1;
 
             } else {
-                console.log(feature.names.common + ": " + file);
+                console.log(placeType + ": " + feature.id + ": " + feature.names.common + ": " + squareKm + ": " + file.slice(58));
             }
 
             return callback();
