52f71ac314f65be807aebe4ed25b9d521d75068d
Tim Park <timfpark@gmail.com>
2016-12-20 12:39:39 -0800

    remove debug message

--- controllers/intersection.js
+++ controllers/intersection.js
@@ -5,7 +5,6 @@ const common = require('service-utils'),
       services = require('../services');
 
 exports.post = function(req, res) {
-    common.services.log.info('intersection received: ' + JSON.stringify(req.body.intersection, null, 2));
     services.visits.updateVisitsFromIntersection(req.body.intersection, err => {
         if (err) return common.utils.handleError(res, err);
 
